e9067cdd284f99e37b5f1be8f434694dde777ca5
liuwenwei <liuww06@163.com>
2015-06-30 14:39:51 +0800

    add project: 

---
+++ TouchTyping/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TouchTyping
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        LinearGradientBrush pressBruch; 
+        public MainWindow()
+        {
+            InitializeComponent();
+            pressBruch = this.FindResource("keyPressColor") as LinearGradientBrush;
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            Button btn = this.keyBoardGrid.FindName("btn" + e.Key.ToString().ToUpper()) as Button;
+            if (btn!=null)
+            {
+                btn.Background = pressBruch;
+            }
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            Button btn = this.keyBoardGrid.FindName("btn" + e.Key.ToString().ToUpper()) as Button;
+            if (btn != null)
+            {
+                btn.Background = null;
+            }
+        }
+    }
+}

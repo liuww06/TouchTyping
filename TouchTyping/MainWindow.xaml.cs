c1a8a562de8871a82e11cfd784ac6f09f0c92bae
liuwenwei <liuww06@163.com>
2015-06-30 22:18:59 +0800

    add animation

--- TouchTyping/MainWindow.xaml.cs
+++ TouchTyping/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Media.Animation;
 
 namespace TouchTyping
 {
@@ -20,11 +21,21 @@ namespace TouchTyping
     /// </summary>
     public partial class MainWindow : Window
     {
-        LinearGradientBrush pressBruch; 
+        LinearGradientBrush pressBruch;
+        DoubleAnimation fontSizeAnimation = new DoubleAnimation();
         public MainWindow()
         {
             InitializeComponent();
             pressBruch = this.FindResource("keyPressColor") as LinearGradientBrush;
+            fontSizeAnimation.From = 20;
+            fontSizeAnimation.To = 80;
+            fontSizeAnimation.Duration = TimeSpan.FromSeconds(0.7);
+            fontSizeAnimation.Completed += fontSizeAnimation_Completed;
+        }
+
+        void fontSizeAnimation_Completed(object sender, EventArgs e)
+        {
+            this.labTip.Content = string.Empty;
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
@@ -33,6 +44,8 @@ namespace TouchTyping
             if (btn!=null)
             {
                 btn.Background = pressBruch;
+                this.labTip.Content = e.Key.ToString().ToUpper();
+                this.labTip.BeginAnimation(Label.FontSizeProperty, fontSizeAnimation);
             }
         }
 
